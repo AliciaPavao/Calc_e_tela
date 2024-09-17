b861617a946e0d138c0e3fc92fafd6d35745a370
AliciaPavao <aliciapavao369@gmail.com>
2024-09-17 10:20:45 -0300

    exclusão do form1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Calc_e_tela
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new splash());
         }
     }
 }

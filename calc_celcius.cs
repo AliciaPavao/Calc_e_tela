6ac122e4337576df64c907a3d485d62a7415a370
Guilherme Mitsuyuki <guilherme.mitsuyuki@gmail.com>
2024-10-01 08:55:18 -0300

    Design da tela de calcular celcius

---
+++ calc_celcius.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calc_e_tela
+{
+    public partial class calc_celcius : Form
+    {
+        public calc_celcius()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
